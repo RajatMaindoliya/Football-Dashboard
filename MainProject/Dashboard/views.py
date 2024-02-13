463f7119602fee5b0edb6d14a8ad4634f0a24e9f
RajatMaindoliya <rajatmaindoliya13@gmail.com>
2024-02-13 16:18:32 +0000

    added registration, login and logout functionality

--- MainProject/Dashboard/views.py
+++ MainProject/Dashboard/views.py
@@ -5,7 +5,7 @@ import requests
 
 def index(request):
     # Make API call to fetch league standings data
-    standings_url = "https://apiv3.apifootball.com/?action=get_standings&league_id=152&APIkey=9c9749f2a2029cae173b134f0587a2761af39a48300c0627414b0181e7a32251"
+    standings_url = "https://apiv3.apifootball.com/?action=get_standings&league_id=152&APIkey=99c096fc17be683fe159341eb603465beca42fd6fe0df1843e51191b47f67d1d"
     standings_response = requests.get(standings_url)
     standings_data = standings_response.json()
     
@@ -13,12 +13,12 @@ def index(request):
     today = datetime.today().strftime('%y-%m-%d')
     
     # make API call to get events data
-    events_url = "https://apiv3.apifootball.com/?action=get_events&from=2024-01-22&to=2024-01-22&league_id=152&APIkey=9c9749f2a2029cae173b134f0587a2761af39a48300c0627414b0181e7a32251"
+    events_url = "https://apiv3.apifootball.com/?action=get_events&from=2024-01-22&to=2024-01-22&league_id=152&APIkey=99c096fc17be683fe159341eb603465beca42fd6fe0df1843e51191b47f67d1d"
     events_response = requests.get(events_url)
     events_data = events_response.json() #turning the text result into json (array)
     
     # Make API call to get top scorer data
-    topScorer_url = "https://apiv3.apifootball.com/?action=get_topscorers&league_id=152&APIkey=9c9749f2a2029cae173b134f0587a2761af39a48300c0627414b0181e7a32251"
+    topScorer_url = "https://apiv3.apifootball.com/?action=get_topscorers&league_id=152&APIkey=99c096fc17be683fe159341eb603465beca42fd6fe0df1843e51191b47f67d1d"
     topScorer_response = requests.get(topScorer_url)
     topScorer_data = topScorer_response.json()
 
