0ed51db9dd060a96fcb12b0e7d53700c12185fd2
RajatMaindoliya <rajatmaindoliya13@gmail.com>
2024-03-02 23:51:01 +0000

    changed landing page

--- MainProject/MainProject/urls.py
+++ MainProject/MainProject/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Dashboard.urls')),
     path('', include('Authentication.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
