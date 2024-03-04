f69ca7b1223f9686335535bf01c84eb0d920a70f
RajatMaindoliya <rajatmaindoliya13@gmail.com>
2024-03-04 23:56:16 +0000

    added fixtures page

--- MainProject/Dashboard/urls.py
+++ MainProject/Dashboard/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('dashboard', views.display_dashboard, name='dashboard'),
-    path('topscorer', views.display_topscorer_list, name='topscorer')
+    path('topscorer', views.display_topscorer_list, name='topscorer'),
+    path('fixtures', views.display_fixtures, name='fixtures')
 ]
-
-#https://apiv3.apifootball.com/?action=get_events&from=2023-04-05&to=2023-04-24&league_id=152&APIkey=9c9749f2a2029cae173b134f0587a2761af39a48300c0627414b0181e7a32251
