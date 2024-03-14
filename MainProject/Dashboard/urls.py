1ae4f7de573764c960853ae3b3386d8a23b10dec
RajatMaindoliya <rajatmaindoliya13@gmail.com>
2024-03-14 15:27:52 +0000

    added match details page

--- MainProject/Dashboard/urls.py
+++ MainProject/Dashboard/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('dashboard', views.display_dashboard, name='dashboard'),
     path('topscorer', views.display_topscorer_list, name='topscorer'),
-    path('fixtures', views.display_fixtures, name='fixtures')
+    path('fixtures', views.display_fixtures, name='fixtures'),
+    path('match_details/<str:match_id>/', views.display_match_details, name='match_details'),
 ]
