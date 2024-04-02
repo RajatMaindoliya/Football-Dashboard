31bae14aa37fa7745924c261faa5e59e66cb3e4c
RajatMaindoliya <rajatmaindoliya13@gmail.com>
2024-04-03 00:58:01 +0100

    name change and added neural network model

--- MainProject/Predictions/models.py
+++ MainProject/Predictions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import pickle
 
-# Create your models here.
+class FootballPredictor:
+    def __init__(self):
+        with open('Model/neural_network_model.pkl', 'rb') as f:
+            self.model = pickle.load(f)
+            
+    def predict_outcome(self, team_performance, weather_conditions):
+        prediction = self.model.predict([team_performance, weather_conditions])
+        return prediction
